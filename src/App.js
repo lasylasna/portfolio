0774b2a8e36b000fa2ccb931a61d4b15b0b93cb2
lasylasna <lasylasna@gmail.com>
2021-05-15 18:40:20 +0400

    nogh_pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Particles from "react-particles-js";
-import Navbar from "./components/Navbar";
+import NavBar from "./components/NavBar";
  import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
   import Services from "./components/Services";
@@ -35,7 +35,7 @@ function App() {
           }
         }}
       />
-      <Navbar />
+      <NavBar />
   <Header />
  <AboutMe />
   <Services />
